a0ec7f3e32262c26a9192f623cb2133e90778eb6
JostesMarcel <jostes77@googlemail.com>
2022-01-18 16:02:01 +0100

    JS1.2_InnerHTML

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let info = document.getElementById("info");
+info.innerHTML = ("<h1> Hello World </h1>")
+info.innerHTML += ("<h2> How are you?")
+console.log(info);
+
+let container = document.getElementById("container")
+container.innerHTML = ("<p>start of the element")
+document.write ("end of the element")
